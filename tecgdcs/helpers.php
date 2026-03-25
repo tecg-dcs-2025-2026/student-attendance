9f750dfba7f56833df57b310d52dd9093663d47e
Dominique Vilain <dominiquevilain@users.noreply.github.com>
2026-03-25 16:38:50 +0100

    feat: add real effects for DB impacting actions

--- tecgdcs/helpers.php
+++ tecgdcs/helpers.php
@@ -62,6 +62,10 @@ if (!function_exists('view')) {
 if (!function_exists('csrf_token')) {
     function csrf_token(int $length = 32): string
     {
-        return $_SESSION['token'] ?? bin2hex(random_bytes($length));
+        if (!isset($_SESSION['token'])) {
+            $_SESSION['token'] = bin2hex(random_bytes($length));
+        }
+
+        return $_SESSION['token'];
     }
 }
