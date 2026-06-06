21de2a2445d8b73b9033cb6e48f729b93a24ad48
Dominique Vilain <dominiquevilain@users.noreply.github.com>
2026-06-06 12:20:07 +0200

    add full CourseControllerTest suite

---
+++ tests/Feature/CourseControllerTest.php
@@ -0,0 +1,138 @@
+<?php
+
+use App\Models\Course;
+use App\Models\User;
+
+it(
+    'has an index route for courses',
+    function () {
+        $user = User::factory()->create();
+
+        // Redirect if not authenticated
+        $response = $this->get(route('courses.index'));
+
+        $response->assertStatus(302);
+
+        // Ok if authenticated
+        \Pest\Laravel\actingAs($user);
+
+        $response = $this->get(route('courses.index'));
+
+        $response->assertStatus(200);
+    }
+);
+
+it(
+    'passes a title to the index view',
+    function () {
+        $user = User::factory()->create();
+        \Pest\Laravel\actingAs($user);
+
+        $response = $this->get(route('courses.index'));
+
+        $response->assertViewHas('title');
+    }
+);
+
+it(
+    'has a show route for courses',
+    function () {
+        $user = User::factory()->create();
+        $course = Course::factory()
+            ->for($user)
+            ->create();
+
+        // Redirect if not authenticated
+        $response = $this->get(route('courses.show', $course));
+
+        $response->assertStatus(302);
+
+        // Ok if authenticated
+        \Pest\Laravel\actingAs($user);
+
+        $response = $this->get(route('courses.show', $course));
+
+        $response->assertStatus(200);
+    }
+);
+
+it(
+    'passes a title to the show view',
+    function () {
+        $user = User::factory()
+            ->hasCourses()
+            ->create();
+        \Pest\Laravel\actingAs($user);
+        $course = $user->courses()->first();
+
+        $response = $this->get(route('courses.show', $course));
+
+        $response->assertViewHas('title', $course->name);
+    }
+);
+
+it(
+    'shows a user his courses',
+    function () {
+        $user1 = User::factory()
+            ->hasCourses()
+            ->create();
+        $user2 = User::factory()
+            ->hasCourses()
+            ->create();
+        \Pest\Laravel\actingAs($user1);
+
+        $response = $this->get(route('courses.index'));
+
+        $response->assertSee($user1->courses->first()->name);
+    }
+);
+
+it(
+    'does not show a user the courses of another user',
+    function () {
+        $user1 = User::factory()
+            ->hasCourses()
+            ->create();
+        $user2 = User::factory()
+            ->hasCourses()
+            ->create();
+        \Pest\Laravel\actingAs($user1);
+
+        $response = $this->get(route('courses.index'));
+
+        $response->assertDontSee($user2->courses->first()->name);
+    }
+);
+
+it(
+    'displays the courses in the ascending order of course names',
+    function () {
+        $user1 = User::factory()
+            ->hasCourses(10)
+            ->create();
+        \Pest\Laravel\actingAs($user1);
+
+        $response = $this->get(route('courses.index'));
+
+        $response->assertSeeInOrder($user1->courses->sortBy('name')->pluck('name')->toArray());
+    }
+);
+
+it(
+    'displays the courses with the count of enrolled students',
+    function () {
+        $user1 = User::factory()
+            ->has(
+                Course::factory()
+                    ->hasStudents()
+            )
+            ->create();
+        \Pest\Laravel\actingAs($user1);
+        $student_count = $user1->courses()->first()->students()->count();
+
+        $response = $this->get(route('courses.index'));
+
+        $response->assertSee($student_count.' '.__('nouns.student(s)'));
+    }
+);
